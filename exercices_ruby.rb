d34543c65b3f266c5fec2a9ec305458a479451ec
ThomasLaneyrie <thomas.laneyrie@gmail.com>
2023-01-17 18:05:38 +0100

    Exo17

--- exercices_ruby.rb
+++ exercices_ruby.rb
@@ -1,24 +1,51 @@
- #ruby exo_16.rb
+#ruby exo_17.rb
+# Exo 17 : j'ai suivi une logique où j'ai analysé la pyramide et où devait être quoi pour savoir dans chaque elem le nombre de blanc et d'étoile. Après j'ai assemblé.
+# Longueur de la dernière ligne = (nombre_de_colonne * 2)-1
 	puts "De combien d'étages souhaites-tu avoir ta pyramide ? Donne moi un nombre entre 1 et 25"
 	number = gets.chomp.to_i
+	nombre_carac=number*2 -1
 	arr=Array.new()
-	
-	if number >= 1 && number <=25 then
-		for num_ligne in 1..number
-			element = ""
-			for i in 1..num_ligne
-				element = "#" + element
-			end
-			for i in 1..(number - num_ligne)
-				element = " " + element
-			end
-			arr.push(element)
+
+	for num_ligne in 1..number
+		element = ""
+		nombre_etoile_ligne = 2*num_ligne -1
+		nombre_blanc = (nombre_carac-nombre_etoile_ligne)/2
+		for i in 1..nombre_blanc 
+			element = " " + element
 		end
-		puts "Voici ta pyramide :"
-		puts arr
-	elsif 
-		puts "Je t'ai dit entre 1 et 25, vas-y recommence"
+		for i in 1..nombre_etoile_ligne
+			element = element + "#"
+		end
+		for i in 1..nombre_blanc 
+			element = element + " "
+		end
+		arr.push(element)
 	end
+	puts arr
+
+
+#  #ruby exo_16.rb
+# Exo 16 : j'au suivi une logique ou la 1ere boucle for poussait 1 étoile au fond. La 2eme boucle for -> 2 étoiles au fond. Etc.
+# 	puts "De combien d'étages souhaites-tu avoir ta pyramide ? Donne moi un nombre entre 1 et 25"
+# 	number = gets.chomp.to_i
+# 	arr=Array.new()
+	
+# 	if number >= 1 && number <=25 then
+# 		for num_ligne in 1..number
+# 			element = ""
+# 			for i in 1..num_ligne
+# 				element = "#" + element
+# 			end
+# 			for i in 1..(number - num_ligne)
+# 				element = " " + element
+# 			end
+# 			arr.push(element)
+# 		end
+# 		puts "Voici ta pyramide :"
+# 		puts arr
+# 	elsif 
+# 		puts "Je t'ai dit entre 1 et 25, vas-y recommence"
+# 	end
 	
 
 	# if number >= 1 && number <=25 then
