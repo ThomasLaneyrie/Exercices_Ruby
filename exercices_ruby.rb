399dcb51510c9da9e2bdd37bbad8b1e347b670ea
ThomasLaneyrie <thomas.laneyrie@gmail.com>
2023-01-17 11:12:34 +0100

    Ajout des exos jusqu'au 11

--- exercices_ruby.rb
+++ exercices_ruby.rb
@@ -1,12 +1,60 @@
+#ruby exo_11.rb
+	puts "Quelle est ton année de naissance ?"
+	birth_year = gets.chomp.to_i
+	current_year = 2023
+	i = current_year - birth_year + 1 
+	age = 0
+	increment = current_year - birth_year 
+
+	i.times do
+		if birth_year != 2023 then
+			puts "En #{birth_year}, il y a #{increment} ans, tu avais #{age} ans"
+			birth_year = birth_year + 1
+			age = age + 1
+			increment = increment - 1
+		elsif birth_year == 2023 then
+			puts "Et cette année 2023, tu as #{age} ans !"
+		end
+	end	
+
+#ruby exo_10.rb
+	# puts "Quelle est ton année de naissance ?"
+	# birth_year = gets.chomp.to_i
+	# current_year = 2023
+	# i = current_year - birth_year + 1 
+	# age = 0
+
+	# i.times do
+	# 	if birth_year != 2023 then
+	# 		puts "En #{birth_year} : tu avais #{age} ans"
+	# 		birth_year = birth_year + 1
+	# 		age = age + 1
+	# 	elsif birth_year == 2023 then
+	# 		puts "En 2023 : tu as ou va avoir #{age} ans"
+	# 	end
+	# end
+
+#ruby exo_09.rb
+	# puts "Quelle est ton année de naissance ?"
+	# birth_year = gets.chomp.to_i
+
+	# puts "OK je vais lister toutes les années depuis ta naissance (Espece de vieux) :"
+	# current_year = 2023
+	# i = current_year - birth_year + 1 
+	# i.times do
+	# 	puts birth_year
+	# 	birth_year = birth_year + 1
+	# end
+
 #ruby exo_08.rb
-    puts "Quel est le compte à rebours ?"
-    number = gets.chomp.to_i
-    i = number
-		j = number + 1
-		j.times do
-			puts i
-			i = i-1
-    end
+    # puts "Quel est le compte à rebours ?"
+    # number = gets.chomp.to_i
+    # i = number
+		# j = number + 1
+		# j.times do
+		# 	puts i
+		# 	i = i-1
+    # end
 
 #ruby exo_07.rb
     # puts "Jusqu'à combien sais-tu compter ?"
