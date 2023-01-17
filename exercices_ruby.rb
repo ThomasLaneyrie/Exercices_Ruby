cd445748c2bfe04797d6f1cb9119a9f80ff6218f
ThomasLaneyrie <thomas.laneyrie@gmail.com>
2023-01-17 10:30:04 +0100

    Ajout exercices 4 et 5

--- exercices_ruby.rb
+++ exercices_ruby.rb
@@ -1,29 +1,53 @@
-# #ruby exo_01.rb
-    # puts "Bonjour, monde !"
+
+#ruby exo_05.rb
+    puts "Combien de fois veux-tu chanter la vie aujourd'hui ?"
+    number = gets.chomp.to_i
+    number.times do
+        puts "Salut, ca farte ?"
+    end
 
 
-# #ruby exo_02.rb
+#ruby exo_04.rb
+    # puts "Quelle est ton année de naissance ?"
+    # year = gets.chomp.to_i
+
+    # hundred_year = year + 100
+    # puts "Tu auras 100 ans en ... #{hundred_year} !"
+
+
+
+#ruby exo_03.rb
+    # puts "Quelle est ton année de naissance ?"
+    # year = gets.chomp.to_i
+    # puts "Quel est ton mois de naissance (1 pour Janvier, 2 pour Février,...) ?"
+    # month = gets.chomp.to_i
+    # puts "Quel est ton jour de naissance ?"
+    # day = gets.chomp.to_i
+
+    # current_year = 2023
+    # difference = 2023-2017+1
+
+    # if month == 1 then
+    #     if day <=17 then
+    #         difference = difference - 1
+    #     end
+    # end
+
+    # age_year = current_year - year - difference
+    # puts "Etant donné que nous sommes les 17/01/2023, tu as (roulement de tambour).... #{age_year} ans !"
+
+
+
+#ruby exo_02.rb
     # puts "Quel est ton prénom ?"
     # prenom = gets.chomp
     # puts "Bonjour, #{prenom} !"
 
 
-#ruby exo_03.rb
-    puts "Quelle est ton année de naissance ?"
-    year = gets.chomp.to_i
-    puts "Quel est ton mois de naissance (1 pour Janvier, 2 pour Février,...) ?"
-    month = gets.chomp.to_i
-    puts "Quel est ton jour de naissance ?"
-    day = gets.chomp.to_i
 
-    current_year = 2023
-    difference = 2023-2017+1
+#ruby exo_01.rb
+    # puts "Bonjour, monde !"
 
-    if month == 1 then
-        if day <=17 then
-            difference = difference - 1
-        end
-    end
 
-    age_year = current_year - year - difference
-    puts "Etant donné que nous sommes les 17/01/2023, tu as (roulement de tambour).... #{age_year} ans !"
+
+
